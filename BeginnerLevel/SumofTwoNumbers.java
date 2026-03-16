eeeda027d2b269f268c0b798feef5934d5f56ade
Shubham <shubhamkr565@gmail.com>
2026-03-16 23:20:33 +0530

    Beginner Level Coding in java

---
+++ BeginnerLevel/SumofTwoNumbers.java
@@ -0,0 +1,26 @@
+// public class SumofTwoNumbers{
+//     public static void main(String[] args){
+//         int a  = 10; 
+//         int b = 20;
+//         int sum = a+b;
+
+//         System.out.println("Sum of two number :"+sum);
+//     }
+// }
+
+import java.util.Scanner;
+
+public class SumofTwoNumbers{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter first number: ");
+        int a = sc.nextInt();
+        System.out.println("Enter second number: ");
+        int b = sc.nextInt();
+
+        int sum = a+b;
+        System.out.println("The sum of "+a + " + " + b+ " = " +sum );
+        sc.close();
+    }
+}
